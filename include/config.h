e346538c55bcd95677e7ccbf0c3789cad74a34b8
gambada <daniele.gambaletta@studio.unibo.it>
2023-06-28 15:29:03 +0200

    chore: add updateData task to store periodically environmental data to flash

--- include/config.h
+++ include/config.h
@@ -7,6 +7,14 @@
 // File name
 #define FILENAME "/env_data.json"
 
+// Task timer
+#define mS_TO_S_FACTOR 1000
+#define uS_TO_S_FACTOR 1000000
+#define TIME_TO_SLEEP UINT32_C(60 * 60 * uS_TO_S_FACTOR)
+
+// Task status
+#define STATUS_DEAD 0
+#define STATUS_ALIVE 1
 
 // Errors
 enum battery_error { BATTERY_OK, BATTERY_LOW };
@@ -16,6 +24,7 @@ enum gps_error { GPS_OK, GPS_CANT_LOCATE };
 enum qrCode_error { QR_OK, QR_CANT_CREATE };
 enum sensor_error { SENSOR_OK, SENSOR_CANT_READ };
 
-
+// Pins
+#define LED_BUILTIN 12
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,60 @@
 #include "config.h"
 #include "devices/EnvSensor.h"
+#include "model/Device.h"
 #include "model/EnvData.h"
+#include "utility/DeviceDataHandler.h"
 #include "utility/Logger.h"
 
 EnvSensor envSensor;
 LoggerService Logger;
 
-void setup() {
-    Logger.init();
-    Logger.log(".:: Init Device ::.");
-    envSensor.init();
+TaskHandle_t UpdateDataTask;
+Device* device;
+
+static volatile uint8_t dataTaskStatus = STATUS_DEAD;
+
+void updateData(void* parameter) {
+    Logger.log("- BORN ENV DATA");
+    dataTaskStatus = STATUS_ALIVE;
+    if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER) {
+        Logger.log("- ENV DATA UPDATE");
+        DeviceDataHandler::updateEnvData(envSensor.getEnvData(), device);
+    }
+    Logger.log("- DEAD ENV DATA");
+    dataTaskStatus = STATUS_DEAD;
+    vTaskDelete(UpdateDataTask);
 }
 
-void loop() { 
-    
+void setup() {
+    Logger.init(true);
+    Logger.log(".:: SETUP Device ::.");
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP);
+
+    envSensor.init();
+
+    device = new Device();
+
+    DeviceDataHandler::init();
+
+    if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_UNDEFINED) {
+        device->setup();
+        DeviceDataHandler::initData(device);
+    }
+
+    // Create update data task
+    xTaskCreatePinnedToCore(updateData, "updateDataTask", 10000, NULL, 10,
+                            &UpdateDataTask, 0);
+
+    // Wait task to finish
+    delay(100);
+    while (dataTaskStatus == STATUS_ALIVE) {
+    }
+
+    Logger.log(".:: SLEEP Device ::.");
+    esp_deep_sleep_start();
 }
+
+void loop() {}

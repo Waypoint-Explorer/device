89e71aac75aed889eee4b726c681c765f71426a7
gambada <daniele.gambaletta@studio.unibo.it>
2023-07-01 03:00:30 +0200

    chore: update function uses of DeviceDataHandler and Gps in main

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,17 @@
 #include "utility/DeviceDataHandler.h"
 #include "utility/Logger.h"
 
-EnvSensor envSensor;
-Gps gps;
 LoggerService Logger;
 Preferences preferences;
-TimeData timeData;
-TaskHandle_t UpdateDataTask;
-static volatile uint8_t dataTaskStatus = STATUS_DEAD;
 
 Device* device;
-
+EnvSensor envSensor;
+Gps gps;
+TimeData timeData;
 Button btnReset(RESET_BUTTON, TIME_LONGPRESS_RESET);
+
+TaskHandle_t UpdateDataTask;
+static volatile uint8_t dataTaskStatus = STATUS_DEAD;
 
 // Function called by UpdateDataTask to update environmental data
 void updateData(void* parameter) {
@@ -30,7 +30,9 @@ void updateData(void* parameter) {
         device->errorsHandler->gps = gps.getGpsData(timeData);
 
         Logger.log("- ENV DATA UPDATE");
-        DeviceDataHandler::writeLastEnvData(envSensor.getEnvData(), device);
+
+        DeviceDataHandler::writeLastEnvData(
+            EntryData(envSensor.getEnvData(), timeData.getTimestamp()), device);
     }
     Logger.log("- DEAD ENV DATA");
     dataTaskStatus = STATUS_DEAD;
@@ -46,13 +48,12 @@ void setup() {
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP);
 
     preferences.begin("device");
-    DeviceDataHandler::begin();
 
     device = new Device();
-
-    gps.init();
     timeData = TimeData();
 
+    DeviceDataHandler::begin();
+    gps.begin();
     device->errorsHandler->sensor = envSensor.begin();
 
     // Check if wake up cause is undefined
@@ -79,16 +80,22 @@ void setup() {
             device->errorsHandler->gps =
                 gps.getGpsData(timeData, device->position);
 
+            // Save preferences
             preferences.putString("id", device->id);
             preferences.putBool("init", true);
-            // envSensor.calibrate(20);
+            preferences.putFloat("latitude", device->position->latitude);
+            preferences.putFloat("longitude", device->position->longitude);
+            envSensor.calibrate(20);
         }
 
         Logger.log(".:: SLEEP Device ::.");
         esp_deep_sleep_start();
     }
 
+    // Get data from Preferences
     device->id = preferences.getString("id");
+    device->position->latitude = preferences.getFloat("latitude");
+    device->position->longitude = preferences.getFloat("longitude");
 
     preferences.end();
 
@@ -101,7 +108,7 @@ void setup() {
     delay(100);
     while (dataTaskStatus == STATUS_ALIVE) {
     }
-    
+
     device->log();
 
     Logger.log(".:: SLEEP Device ::.");
